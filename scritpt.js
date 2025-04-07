a12e755932646e8c96573c4a1defdc575811bc8f
ferreiralc <luizinha.campos@hotmail.com>
2025-04-07 16:24:10 -0300

    Primeiro commit do manual do usuário

---
+++ scritpt.js
@@ -0,0 +1,10 @@
+// Script básico para um possível futuro uso (exemplo de scroll suave)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
